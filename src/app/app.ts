3298daba2630cc48dd431915b193aef19fcd4422
azhan14 <azhanpatel77@gmail.com>
2026-01-27 13:48:49 +0530

    Mat icon rendering issue

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,4 +1,4 @@
-import { Component, signal } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
 import { RouterModule, RouterOutlet } from '@angular/router';
 import { NavbarComponent } from "./layout/navbar-component/navbar-component";
 import { FooterComponent } from "./layout/footer-component/footer-component";
@@ -6,18 +6,22 @@ import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconRegistry } from '@angular/material/icon';
 import { DomSanitizer } from '@angular/platform-browser';
+import { CommonModule } from "@angular/common";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarComponent, FooterComponent, MatSidenavModule, MatButtonModule, RouterModule],
+  imports: [RouterOutlet, NavbarComponent, FooterComponent, MatSidenavModule, MatButtonModule, RouterModule, CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
-export class App {
+export class App implements OnInit {
   protected readonly title = signal('azhan-portfolio');
+  iconRegistered = false;
 
-  constructor(private matIconRegistry: MatIconRegistry, private domSanitizer: DomSanitizer) {
-
+  constructor(private matIconRegistry: MatIconRegistry, private domSanitizer: DomSanitizer) {}
+  
+  
+  ngOnInit(): void {
     this.matIconRegistry.addSvgIcon(
       'github',
       this.domSanitizer.bypassSecurityTrustResourceUrl('assets/github.svg')
@@ -32,5 +36,7 @@ export class App {
       'email',
       this.domSanitizer.bypassSecurityTrustResourceUrl('assets/envelope.svg')
     );
+
+    this.iconRegistered = true;
   }
 }
